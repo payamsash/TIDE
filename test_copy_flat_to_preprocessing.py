9f893566c37e81420a7c0fca9475178fa48f2f61
Beat Tödtli <beat.toedtli@ost.ch>
2025-03-25 20:44:32 +0100

    Adding comments

--- test_copy_flat_to_preprocessing.py
+++ test_copy_flat_to_preprocessing.py
@@ -1,3 +1,5 @@
 from main import copy_flat_to_preprocessing_file_structure
+#first argument: path to a local directory where .eeg, .vmrk and .vhdr-files of several patients are stored.
+#second argument: a directory that will be written to- ideally non-existent
 copy_flat_to_preprocessing_file_structure('~/Tinnitus/Data/OneDrive_1_24.2.2025/project_n/','./testout/')
 
